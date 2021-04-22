8dfed3fcb7444557adfbe83f4910db1bb9ab87e6
Omoyi <urakhel7@gmail.com>
2021-04-22 05:04:58 +0200

    completes the header section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import Header from '../src/components/sections/Header.js'
+import Header from '../src/components/sections/Header.js';
+import Herosection from './components/sections/HeroSection';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Herosection/>
     </div>
   );
 }
---
+++ src/components/MainButton.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    btnSpace: {
+        display: 'inline-block',
+        flexDirection: 'row',
+        alignItems: 'center',
+        position: 'absolute',      
+        left: '655px',
+        top: '523px',
+        boxSizing: 'border-box',
+    },
+    button: { 
+        padding: '10px, 31px',
+        width: '177px',
+        height: '49px',     
+        textAlign: 'center',
+        textDecoration: 'none',
+        display: 'inline-block',
+        fontSize: '17px',
+        lineHeight: '29px',
+        letterSpacing: '0.125em',
+        cursor: 'pointer', 
+        backgroundColor: '#96D2AA', 
+        color: '#0100CD', 
+        border: '1px solid #0100CD',
+        '&:hover': {
+            background: "#0100CD",
+            color: 'white'
+         },
+    },
+}));
+
+function MainButton() {
+    const classes = useStyles();
+    return (
+        <div className={classes.btnSpace}>
+            <button className={classes.button}>
+                Get in touch
+            </button>
+        </div>
+    )
+}
+
+export default MainButton
