367e2a944523ffcf5a7623cfa67a3206e90f99c9
Omoyi <urakhel7@gmail.com>
2021-04-22 02:03:35 +0200

    adds a nav-bar|header

---
+++ src/components/sections/Header.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Logo from '../../Logo.svg'
+import { Link } from 'react-scroll'
+import '../../header.css'
+
+function Header() {
+    return (
+        <div>
+            <nav>
+                <Link to='main'><img src={Logo}/></Link>
+                <ul className='menu' >
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/process">Process</Link></li>
+                    <li><Link to="/solutions">Solutions</Link></li>
+                    <li><Link to="/caseStudies">Case studies</Link></li>
+                    <li><Link to="/impact">Impact</Link></li>
+                    <li><Link to="/contact">Contact</Link></li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Header
